80d8dbf1d02aa8d9bb25982581bb2f74be58bb54
Fakhrii Habibillah An Naufal <fha.naufal06@gmail.com>
2021-11-16 22:30:37 +0700

    get and create department

--- routes/route.go
+++ routes/route.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"net/http"
+	c "presence-app-backend/controllers"
 
 	"github.com/labstack/echo/v4"
 )
@@ -9,12 +10,18 @@ import (
 func New() *echo.Echo {
 	e := echo.New()
 
+	// hello world
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]interface{}{
 			"message": "success",
 			"data":    "Hello world",
 		})
 	})
+
+	// deparments
+	eDepartment := e.Group("/departments")
+	eDepartment.GET("", c.GetDepartmentController)
+	eDepartment.POST("", c.CreateDepartmentController)
 
 	return e
 }
